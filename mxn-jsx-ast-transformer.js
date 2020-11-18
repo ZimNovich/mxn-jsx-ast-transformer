2686418bc54dc59b1631f460993e3ec051c4e4c9
ZimNovich <ilya@zimnovich.com>
2020-11-18 15:20:33 +0300

    Added all the files

--- mxn-jsx-ast-transformer.js
+++ mxn-jsx-ast-transformer.js
@@ -196,7 +196,7 @@ var MXNJSXConv = function(tree, options)
     return tree;
 };
 
-MXNJSXConv.version = "0.8.5";
+MXNJSXConv.version = "0.8.6";
 
 // export the module
 module.exports = MXNJSXConv;
